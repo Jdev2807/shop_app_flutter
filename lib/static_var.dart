d62a1204685df4a74bf12a29dad4507646bf8c1a
jexk <jdev7k48kc@gmail.com>
2022-03-18 17:03:20 +0700

    hehe alex

---
+++ lib/static_var.dart
@@ -0,0 +1,5 @@
+class static_var{
+  static final String FIREBASE_URL = "https://lazado-fc40f-default-rtdb.asia-southeast1.firebasedatabase.app/";
+  static final String FIREBASE_AUTH_API = "AIzaSyDyALtp5onklxiEQKD1uUfmCr4COfSOLpk";
+}
+// store static variable
